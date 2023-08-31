8cbd24c5a1a6859d9cd0757a61fda612e93641c2
Annurkenova <gann3115@gmail.com>
2023-09-01 01:38:52 +0600

    my code

--- exercise1/problem1/index.js
+++ exercise1/problem1/index.js
@@ -1,5 +1,16 @@
 function numOfDigits(int) {
-  // Your code
-}
+  if (int === 0) {
+    return 1;
+  }
+  
+  int = Math.abs(int); // Для отризательных
 
+  let count = 0;
+  while (int !== 0) {
+    int = Math.floor(int / 10);
+    count++;
+  }
+  
+  return count;
+}
 module.exports = numOfDigits;
