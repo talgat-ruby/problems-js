253a4ba777c8b42f8bac858e937006893e5bd7d0
Annurkenova <gann3115@gmail.com>
2023-09-03 19:31:05 +0600

    change code

--- exercise1/problem3/index.js
+++ exercise1/problem3/index.js
@@ -1,5 +1,9 @@
-function pentagonal() {
-  // Your code
+function pentagonal(n) {
+  if (n === 1) {
+    return 1;
+  } else {
+    return 1 + 5 * (n - 1) * (n - 2) / 2;
+  }
 }
 
 module.exports = pentagonal;
--- exercise1/problem7/index.js
+++ exercise1/problem7/index.js
@@ -1,5 +1,10 @@
-function isPrimitive() {
-  // Your code
+function isPrimitive(value) {
+  return (typeof value !== 'object' && typeof value !== 'function') || value === null;
 }
+
+// Examples
+console.log(isPrimitive(function add(x, y) {
+  return x + y;
+}));
 
 module.exports = isPrimitive;
