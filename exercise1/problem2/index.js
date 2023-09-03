b638115443df536d2209da646150775df3e08f83
Annurkenova <gann3115@gmail.com>
2023-09-03 14:01:16 +0600

    change code

--- exercise1/problem2/index.js
+++ exercise1/problem2/index.js
@@ -1,5 +1,12 @@
-function intWithinBounds() {
-  // Your code
-}
+function intWithinBounds(n, lower, upper) {
+  if (!Number.isInteger(n)) {
+    return false;
+  }
 
+  if (lower <= n && n <= upper) {
+    return true;
+  } else {
+    return false;
+  }
+}
 module.exports = intWithinBounds;
--- exercise1/problem4/index.js
+++ exercise1/problem4/index.js
@@ -1,5 +1,4 @@
-function makeTitleCase() {
-  // Your code
+function makeTitleCase(inputString) {
+  return inputString.split(" ").map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(" ");
 }
-
 module.exports = makeTitleCase;
