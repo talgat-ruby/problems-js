99f0aaeeb27f7a710bde6be8f2353d26873b5a92
Annurkenova <gann3115@gmail.com>
2023-09-03 17:06:25 +0600

    change code

--- exercise1/problem6/index.js
+++ exercise1/problem6/index.js
@@ -1,5 +1,5 @@
-function insertWhitespace() {
-  // Your code
+function insertWhitespace(inputString) {
+  return inputString.replace(/([a-z])([A-Z])/g, '$1 $2');
 }
 
 module.exports = insertWhitespace;
