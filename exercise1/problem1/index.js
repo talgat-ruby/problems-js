394dd8efbd66acd749721d6b9856523ef537e00d
Aituar <baraitrama@gmail.com>
2023-09-04 12:10:03 +0600

    problem

--- exercise1/problem1/index.js
+++ exercise1/problem1/index.js
@@ -1,5 +1,18 @@
 function numOfDigits(int) {
-  // Your code
-}
 
-module.exports = numOfDigits;
+    let count = 0
+    if (int === 0) {
+        return 1
+    }
+    while (int != 0) {
+        n = Math.floor(int / 10)
+        count++
+    }
+    return count
+}
+console.log(numOfDigits(1000)) // 4
+console.log(numOfDigits(12)) // 2
+console.log(numOfDigits(1305981031)) // 10
+console.log(numOfDigits(0)) // 1  
+
+ module.exports = numOfDigits;
