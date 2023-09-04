9549c08790b6a09cc2453795ce612538496c25fa
Annurkenova <gann3115@gmail.com>
2023-09-04 18:20:56 +0600

    change code

--- exercise1/problem8/index.js
+++ exercise1/problem8/index.js
@@ -1,10 +1,12 @@
 function longestTime(h, m, s) {
   const Seconds1 = h * 3600;
   const Seconds2 = m * 60;
-  if (Seconds1 >= Seconds2 && Seconds1 >= s) {
+  if (Seconds1 > Seconds2 && Seconds1 > s) {
     return h;
-  } else if (Seconds2 >= Seconds1 && Seconds2 >= s) {
+  } else if (Seconds2 > Seconds1 && Seconds2 > s) {
     return m;
+  } else if (s > Seconds1 && s > Seconds2) {
+    return s;
   } else {
     return s;
   }
