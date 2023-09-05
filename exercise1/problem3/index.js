60bf6310a0084e83f9e6b79219b128fe34bac9d3
Aituar <baraitrama@gmail.com>
2023-09-05 14:19:36 +0600

    problem

--- exercise1/problem3/index.js
+++ exercise1/problem3/index.js
@@ -1,5 +1,13 @@
-function pentagonal() {
-  // Your code
+function pentagonal(n) {
+  if (n === 1) {
+    return 1;
+  }
+
+  return (5 * Math.pow(n, 2) - 5 * n + 2) / 2;
 }
+console.log(pentagonal(1)) // 1
+console.log(pentagonal(2)) // 6
+console.log(pentagonal(3)) // 16
+console.log(pentagonal(8)) // 141
 
 module.exports = pentagonal;
--- exercise1/problem5/index.js
+++ exercise1/problem5/index.js
@@ -1,5 +1,12 @@
-function removeABC() {
-  // Your code
+function removeABC(str) {
+  if (/[abcABC]/.test(str)) { 
+    return str.replace(/[abcABC]/g, ''); 
+  } else {
+    return null; 
+  }
 }
+console.log(removeABC("This might be a bit hard")) // "This might e  it hrd"
+console.log(removeABC("hello world!")) // null
+console.log(removeABC("")) // null
 
 module.exports = removeABC;
