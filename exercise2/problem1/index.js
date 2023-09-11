e591e146e3647532d723adecb04ac25fda149190
Aituar <47512436+AituarBar@users.noreply.github.com>
2023-09-11 17:39:00 +0600

    Update index.js

--- exercise2/problem1/index.js
+++ exercise2/problem1/index.js
@@ -10,3 +10,5 @@ function arrayOfMultiples(num, length) {
 console.log(arrayOfMultiples(7, 5)) // [7, 14, 21, 28, 35]
 console.log(arrayOfMultiples(12, 10)) // [12, 24, 36, 48, 60, 72, 84, 96, 108, 120]
 console.log(arrayOfMultiples(17, 6)) // [17, 34, 51, 68, 85, 102]
+
+module.exports = arrayOfMultiples;
