db2d93bb7a3b4d033bc56bf254cb83ef151e6680
AituarBar <47512436+AituarBar@users.noreply.github.com>
2023-09-11 11:58:33 +0600

    Add files via upload

---
+++ exercise2/problem5/index.js
@@ -0,0 +1,22 @@
+function getBudgets(n) {
+  const sum = n.reduce((accumulator, object) => {
+    return accumulator + object.budget;
+      }, 0); 
+      return sum
+}  
+console.log(
+    getBudgets([
+      {name: "John", age: 21, budget: 23000},
+      {name: "Steve", age: 32, budget: 40000},
+      {name: "Martin", age: 16, budget: 2700},
+    ])
+  ); // 65700
+console.log(
+    getBudgets([
+      {name: "John", age: 21, budget: 29000},
+      {name: "Steve", age: 32, budget: 32000},
+      {name: "Martin", age: 16, budget: 1600},
+    ])
+  ); // 62600
+
+module.exports = getBudgets;
