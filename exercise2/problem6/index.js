11140c27b524b10503293736c8a46c004add594d
Aituar <47512436+AituarBar@users.noreply.github.com>
2023-09-11 17:35:45 +0600

    Update index.js

--- exercise2/problem6/index.js
+++ exercise2/problem6/index.js
@@ -4,5 +4,8 @@ function mapping(arr) {
     return result;
   }, {});
 }
+console.log(u(["p", "s"])) // { "p": "P", "s": "S" }
+console.log(mapping(["a", "b", "c"])) // { "a": "A", "b": "B", "c": "C" }
+console.log(mapping(["a", "v", "y", "z"])) // { "a": "A", "v": "V", "y": "Y", "z": "Z" }
 
 module.exports = mapping;
