b130a7e4efdf8009e6c1b477150b460104b7e22c
Aituar <47512436+AituarBar@users.noreply.github.com>
2023-09-11 17:41:26 +0600

    Update index.js

--- exercise2/problem4/index.js
+++ exercise2/problem4/index.js
@@ -7,3 +7,5 @@ console.log(numInStr(["1a", "a", "2b", "b"])) // ["1a", "2b"]
 console.log(numInStr(["abc", "abc10"])) // ["abc10"]
 console.log(numInStr(["abc", "ab10c", "a10bc", "bcd"])) // ["ab10c", "a10bc"]
 console.log(numInStr(["this is a test", "test1"])) // ["test1"]
+
+module.exports = numInStr;
